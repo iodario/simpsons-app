26e18b94a6273a6c07e304c8466dbcd5bac0a69d
Dario Turchi <iodariomdq@outlook.com>
2022-10-17 00:17:03 -0300

    fix errors

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,33 @@ import './App.css';
 
 function App() {
 
+  //--guardamos los estados con el useState --
+  //--onChange: cada vez que cambie ese input,
+  // va a crear una f que toma el evento como parametro, y llama a setFilter --
   const [filter, setFilter] = useState("");
+
+
 
   return (
     <div className="principal_container">
       <h1>Characters</h1>
-      <form className="search">
+      <form action="" className="search">
         <label htmlFor="filter">Search:</label>
         <input id="filter" name="filter" type="text" value={filter}
           onChange={(event) => setFilter(event.target.value)}
         />
       </form>
       <div className="cards-container">
-        {simpsonsApi.filter(f => f.character.includes(filter)).map((character, i) => (
-          <Card
-            name={character.character}
-            image={character.image}
-            quote={character.quote}
-            key={i}
-
-          />
-        ))}
+        {simpsonsApi.filter(
+          element => element.character.includes(filter)).map((character, i) => (
+            <Card
+              name={character.character}
+              image={character.image}
+              quote={character.quote}
+              key={i}
+            />
+          ))
+        }
       </div>
     </div>
 
